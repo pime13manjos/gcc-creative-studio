f6ff0c7999ee067107d7f0ce7f1cbeb424437c83
Mariana <m.anjos@accenture.com>
2026-03-12 19:56:25 +0000

    fix range

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -39,5 +39,5 @@ resource "google_vpc_access_connector" "connector" {
   name          = "run-sql-connector"
   region        = var.gcp_region
   network       = google_compute_network.main.id
-  ip_cidr_range = "10.8.0.0/28" # Must be a unique /28 range
+  ip_cidr_range = "10.8.1.0/28" # Must be a unique /28 range
 }
