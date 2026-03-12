47020738b7be7cd99be0beb0cda10eeeff7680e7
nunomorais96 <nuno.daniel.morais@accenture.com>
2026-03-12 17:29:32 +0000

    commenting connector resource as the resource already exists

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -14,7 +14,7 @@ resource "google_compute_subnetwork" "subnet" {
 
 # 3. Reserve a Private IP range for Google Managed Services (Cloud SQL)
 resource "google_compute_global_address" "private_ip_address" {
-  name          = "google-managed-services-range"
+  name          = "google-managed-services-range2"
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 16
@@ -29,9 +29,9 @@ resource "google_service_networking_connection" "private_vpc_connection" {
 }
 
 # 5. Serverless VPC Access Connector (For Cloud Run)
-resource "google_vpc_access_connector" "connector" {
-  name          = "run-sql-connector"
-  region        = var.gcp_region
-  network       = google_compute_network.main.id
-  ip_cidr_range = "10.8.0.0/28" # Must be a unique /28 range
-}
+# resource "google_vpc_access_connector" "connector" {
+#   name          = "run-sql-connector"
+#   region        = var.gcp_region
+#   network       = google_compute_network.main.id
+#   ip_cidr_range = "10.8.0.0/28" # Must be a unique /28 range
+# }
