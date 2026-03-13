b3f62358ba153b60cd487f66d5ab07c0f5286d68
Mariana <m.anjos@accenture.com>
2026-03-13 14:14:27 +0000

    change ip type

--- backend/src/database.py
+++ backend/src/database.py
@@ -177,7 +177,7 @@ class WorkerDatabase:
                     user=config_service.DB_USER,
                     password=config_service.DB_PASS,
                     db=config_service.DB_NAME,
-                    ip_type=IPTypes.PUBLIC,
+                    ip_type=IPTypes.PRIVATE,
                 )
 
             self.engine = create_async_engine(
