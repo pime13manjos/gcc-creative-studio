51af302f66b8a6e77e8a8fac9c76064c1caa839d
Mariana <m.anjos@accenture.com>
2026-03-20 06:48:07 +0000

    change range

--- infra/modules/platform/main.tf
+++ infra/modules/platform/main.tf
@@ -218,7 +218,7 @@ resource "google_cloud_run_v2_service" "default" {
 
 resource "google_vpc_access_connector" "connector" {
   name          = "run-sql-connector"
-  ip_cidr_range = "10.8.1.0/28" # A small, unused range in your VPC
+  ip_cidr_range = "10.8.2.0/28" # A small, unused range in your VPC
   network       = var.vpc_network_id
   min_instances = 2
   max_instances = 3
